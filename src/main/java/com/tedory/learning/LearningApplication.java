33b83e6a0ecdb1b28d8e613621ede53ea85ce5d3
ted555 <thanong984@gmail.com>
2019-03-20 12:55:40 +0700

    Learn Application Runner and Command Line Runner

--- src/main/java/com/tedory/learning/LearningApplication.java
+++ src/main/java/com/tedory/learning/LearningApplication.java
@@ -1,15 +1,27 @@
 package com.tedory.learning;
 
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @SpringBootApplication
-public class LearningApplication {
+public class LearningApplication implements ApplicationRunner, CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(LearningApplication.class, args);
     }
 
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        System.out.println("Hello from Application Runner!");
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Hello from Command Line Runner!");
+    }
 }
--- src/main/java/com/tedory/learning/SimpleController.java
+++ src/main/java/com/tedory/learning/SimpleController.java
@@ -1,6 +1,5 @@
 package com.tedory.learning;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
