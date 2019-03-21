1403c1c5b3aa4314537b2ccb86b35c04f8ec438f
ted555 <thanong984@gmail.com>
2019-03-21 15:45:56 +0700

    Learning: Building RESTful Web Services

--- src/main/java/com/tedory/learning/ProductServiceController.java
+++ src/main/java/com/tedory/learning/ProductServiceController.java
@@ -1,11 +1,13 @@
 package com.tedory.learning;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -13,6 +15,9 @@ import java.util.Map;
 @RestController
 @RequestMapping("/main_path")
 public class ProductServiceController {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+    @Autowired
+    private ObjectMapper mapper;
     private static Map<String, Product> productRepo = new HashMap<>();
     static {
         Product honey = new Product();
@@ -26,15 +31,37 @@ public class ProductServiceController {
         productRepo.put(almond.getId(), almond);
     }
 
-    //Default HTTP request is GET. not require any Request Body
+    //Default HTTP request is GET method. not require any Request Body
     @RequestMapping(value = "/products", method = RequestMethod.GET)
-    public ResponseEntity<Object> getProduct() {
+    public ResponseEntity<Object> getProduct() throws JsonProcessingException {
+        logger.info("Get products: \n" + mapper.writerWithDefaultPrettyPrinter().writeValueAsString(productRepo.values()));
         return new ResponseEntity<>(productRepo.values(), HttpStatus.OK);
     }
 
-    @RequestMapping(value = "/products", method = RequestMethod.POST)
-    public ResponseEntity<Object> createProduct(@RequestBody Product product) {
+    //Using HTTP request POST. require parameter
+    @RequestMapping(value = "/create_product", method = RequestMethod.POST)
+    public ResponseEntity<Object> createProduct(@RequestBody Product product) throws JsonProcessingException {
         productRepo.put(product.getId(), product);
-        return new ResponseEntity<>("Product is created successfully!", HttpStatus.CREATED);
+        logger.info("Create product: \n" + mapper.writerWithDefaultPrettyPrinter().writeValueAsString(product));
+        return new ResponseEntity<>(productRepo.values(), HttpStatus.CREATED);
+    }
+
+    //Using Using HTTP request PUT method. require parameter
+    @RequestMapping(value = "/update_product/{id}", method = RequestMethod.PUT)
+    public  ResponseEntity<Object> updateProduct(@PathVariable("id") String id, @RequestBody Product product) throws JsonProcessingException {
+        productRepo.remove(id);
+        product.setId(id);
+        productRepo.put(id, product);
+        logger.info("Update product: \n" + mapper.writerWithDefaultPrettyPrinter().writeValueAsString(product));
+        return new ResponseEntity<>(productRepo.values(), HttpStatus.OK);
+    }
+
+    //Using HTTP request DELETE method. no require parameter
+    @RequestMapping(value = "/delete_product/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<Object> deleteProduct(@PathVariable("id") String id) throws JsonProcessingException {
+        Product delProduct = productRepo.get(id);
+        productRepo.remove(id);
+        logger.info("Delete product: \n" + mapper.writerWithDefaultPrettyPrinter().writeValueAsString(delProduct));
+        return new ResponseEntity<>(productRepo.values(), HttpStatus.OK);
     }
 }
